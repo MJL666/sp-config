9e49515045728292daa113121f62a67b7c57e974
MJL666 <348967175@qq.com>
2020-02-07 11:19:40 +0800

    test

---
+++ sp10-turbine/src/test/java/cn/tedu/sp10/Sp100TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp100TurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
